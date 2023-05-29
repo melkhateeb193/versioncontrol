5b9d06dc4065b054aaeeeffa81f98b37b5b3fed1
melkhateeb193 <mostafamagdy8800@gmail.com>
2023-05-29 16:20:26 +0300

    updated mostafa merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { ProductsComponent } from './Components/products/products.component';
 
 const routes: Routes = [
 
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: '/products', component: ProductsComponent, title: 'Productes' },
+
 
 ];
 
