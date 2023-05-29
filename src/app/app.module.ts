1e2dafc36781b291c5cc87175a5fe767dbb08368
melkhateeb193 <mostafamagdy8800@gmail.com>
2023-05-29 15:09:53 +0300

    contactUSComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
